fabb889aa0fdfeb20cf31a5de287dde4470c3f9b
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-05-19 03:51:06 -1000

    roman_numerals (#28)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- lingua_nostra/lang/parse_common.py
+++ lingua_nostra/lang/parse_common.py
@@ -16,6 +16,8 @@
 from collections import namedtuple
 import re
 
+ROMAN_NUMERALS = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
 
 class Normalizer:
     """
--- test/test_parse_common.py
+++ test/test_parse_common.py
@@ -14,11 +14,16 @@
 # limitations under the License.
 
 import unittest
-
+from lingua_nostra import load_language
 from lingua_nostra.lang.parse_common import tokenize, Token
+from lingua_nostra.parse import roman_to_int, is_roman_numeral,\
+    extract_roman_numeral_spans, normalize_roman_numerals
 
 
 class TestParseCommon(unittest.TestCase):
+    def setUp(cls) -> None:
+        load_language("en")
+
     def test_tokenize(self):
         self.assertEqual(tokenize('One small step for man'),
                          [Token('One', 0), Token('small', 1), Token('step', 2),
@@ -33,3 +38,48 @@ class TestParseCommon(unittest.TestCase):
 
         self.assertEqual(tokenize('hashtag #1world'),
                          [Token('hashtag', 0), Token('#1world', 1)])
+
+    def test_roman(self):
+        # valid numerals
+        self.assertEqual(roman_to_int("III"), 3)
+        self.assertEqual(roman_to_int("IV"), 4)
+        self.assertEqual(roman_to_int("V"), 5)
+        self.assertEqual(is_roman_numeral("IV"), True)
+
+        # invalid numerals
+        self.assertEqual(roman_to_int("v"), None)
+        self.assertEqual(is_roman_numeral("ii"), False)
+        self.assertEqual(is_roman_numeral("the IV century"), False)
+
+        # test spans
+        self.assertEqual(extract_roman_numeral_spans("the IV century"),
+                         [(4, (4, 6))])
+        self.assertEqual(extract_roman_numeral_spans("the XIV century"),
+                         [(14, (4, 7))])
+
+        # test normalization
+        self.assertEqual(normalize_roman_numerals("the XV century"),
+                         "the 15 century")
+
+        # test ordinals
+        self.assertEqual(normalize_roman_numerals("the XXI century",
+                                                  ordinals=True),
+                         "the 21st century")
+        self.assertEqual(normalize_roman_numerals("the XII century",
+                                                  ordinals=True),
+                         "the 12nd century")
+        self.assertEqual(normalize_roman_numerals("the XXIII century",
+                                                  ordinals=True),
+                         "the 23rd century")
+        self.assertEqual(normalize_roman_numerals("the XXIV century",
+                                                  ordinals=True),
+                         "the 24th century")
+
+        # test space
+        self.assertEqual(is_roman_numeral("I V"), False)
+        self.assertEqual(normalize_roman_numerals("the X V century"),
+                         "the 10 5 century")
+        self.assertEqual(extract_roman_numeral_spans("the X V century"),
+                         [(10, (4, 5)),
+                          (5, (6, 7))])
+
